247e090bf7a39c91849e2a62f9ae350bdb616a2c
Rainweic <861514706@qq.com>
2024-07-26 16:25:13 +0800

    commit 4

--- ws/src/manager.rs
+++ ws/src/manager.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
+use std::sync::mpsc::SendError;
 use dashmap::DashMap;
 use tokio::sync::mpsc;
+use tracing::{debug, error, info};
 use abi::message::Msg;
 use crate::client::Client;
 
@@ -36,5 +38,40 @@ impl Manager {
         if let Some(user_clients) = self.hub.get_mut(&user_id) {
             user_clients.remove(&platform_id);
         }
+    }
+
+    /// 消息发送给所有客户端
+    async fn send_msg_to_clients(&self, clients: &DashMap<PlatformID, Client>, msg: &Msg) {
+        for client in clients.iter() {
+            let content = match serde_json::to_string(msg) {
+                Ok(res) => res,
+                Err(_) => {
+                    error!("msg serialize error");
+                    return;
+                }
+            };
+            if let Err(e) = client.value().send_text(content).await {
+                error!("msg send error: {:?}", e);
+            }
+        }
+    }
+
+    /// 发送消息到指定user
+    pub async fn send_single_msg(&self, user_id: &UserID, msg: &Msg) {
+        if let Some(clients) = self.hub.get(user_id) {
+            self.send_msg_to_clients(&clients, msg).await
+        }
+    }
+
+    pub async fn run(&mut self, mut receiver: mpsc::Receiver<Msg>) {
+        info!("manager start");
+        while let Some(message) = receiver.recv().await {
+            debug!("receive message: {:?}", message);
+            self.send_single_msg(&message.receiver_id, &message).await;
+        }
+    }
+
+    pub async fn broadcast(&self, msg: Msg) -> Result<(), tokio::sync::mpsc::error::SendError<Msg>> {
+        self.tx.send(msg).await
     }
 }
